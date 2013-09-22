b85255270f6412beffc13aefe0ce856f247a5539
Javi Marina <javmarina@gmail.com>
2013-09-22 20:46:32 +0200

    Changes to SDK levels

--- appinventor/components/src/com/google/appinventor/components/runtime/util/SdkLevel.java
+++ appinventor/components/src/com/google/appinventor/components/runtime/util/SdkLevel.java
@@ -16,18 +16,22 @@ import android.os.Build;
  * @author sharon@google.com (Sharon Perl)
  */
 public class SdkLevel {
-  public static final int LEVEL_CUPCAKE = 3;              // a.k.a. 1.5
-  public static final int LEVEL_DONUT = 4;                // a.k.a. 1.6
-  public static final int LEVEL_ECLAIR = 5;               // a.k.a. 2.0
-  public static final int LEVEL_ECLAIR_0_1 = 6;           // a.k.a. 2.0.1
-  public static final int LEVEL_ECLAIR_MR1 = 7;           // a.k.a. 2.1
-  public static final int LEVEL_FROYO = 8;                // a.k.a. 2.2
-  public static final int LEVEL_GINGERBREAD = 9;          // a.k.a. 2.3
-  public static final int LEVEL_GINGERBREAD_MR1 = 10;     // a.k.a. 2.3.3
-  public static final int LEVEL_HONEYCOMB = 12;           // a.k.a. 3.0
-  public static final int LEVEL_ICE_CREAM_SANDWICH = 14;  // a.k.a. 4.0
-  public static final int LEVEL_JELLYBEAN = 16;           // a.k.a. 4.1
-  public static final int LEVEL_JELLYBEAN_MR1 = 17;       // a.k.a. 4.2
+  public static final int LEVEL_CUPCAKE = 3;                  // a.k.a. 1.5
+  public static final int LEVEL_DONUT = 4;                    // a.k.a. 1.6
+  public static final int LEVEL_ECLAIR = 5;                   // a.k.a. 2.0
+  public static final int LEVEL_ECLAIR_0_1 = 6;               // a.k.a. 2.0.1
+  public static final int LEVEL_ECLAIR_MR1 = 7;               // a.k.a. 2.1
+  public static final int LEVEL_FROYO = 8;                    // a.k.a. 2.2
+  public static final int LEVEL_GINGERBREAD = 9;              // a.k.a. 2.3
+  public static final int LEVEL_GINGERBREAD_MR1 = 10;         // a.k.a. 2.3.3
+  public static final int LEVEL_HONEYCOMB = 11;               // a.k.a. 3.0
+  public static final int LEVEL_HONEYCOMB_MR1 = 12;           // a.k.a. 3.1
+  public static final int LEVEL_HONEYCOMB_MR2 = 13;           // a.k.a. 3.2
+  public static final int LEVEL_ICE_CREAM_SANDWICH = 14;      // a.k.a. 4.0-4.0.2
+  public static final int LEVEL_ICE_CREAM_SANDWICH_MR1 = 15;  // a.k.a. 4.0.3-4.0.4
+  public static final int LEVEL_JELLYBEAN = 16;               // a.k.a. 4.1
+  public static final int LEVEL_JELLYBEAN_MR1 = 17;           // a.k.a. 4.2
+  public static final int LEVEL_JELLYBEAN_MR2 = 18;           // a.k.a. 4.3
 
   private SdkLevel() {
   }
